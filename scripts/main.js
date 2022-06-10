182d4ddf634b045acd82995a9c90c07c9ef8519c
DmitryyParkhomenko <dim.parkhom2007@gmail.com>
2022-06-10 20:56:42 +0200

    It completely works!

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,14 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
     const form_span = document.querySelector('.form-span');
-    const checkbox = document.querySelector('.form-checkbox'); debugger
-    if (checkbox.checked != true) {
-        form_span.addEventListener('click', () => {
-            checkbox.checked = 'true'; debugger
-        });
-    } else {
-        form_span.addEventListener('click', () => {
-            checkbox.checked = 'false'; debugger
-        });
-    }
+    const checkbox = document.querySelector('.form-checkbox'); 
+
+    form_span.addEventListener('click', () => {
+        if (checkbox.checked !== true) {
+            checkbox.checked = true; 
+        } else {
+            checkbox.checked = false; 
+        }
+    });
     
 });
