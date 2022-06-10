0e20692a2f44b196323d24b1977b3673bc1128d3
DmitryyParkhomenko <dim.parkhom2007@gmail.com>
2022-06-10 20:42:51 +0200

    Tried if..else to fix checkbox

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,15 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('DOM fully loaded and parsed');
 
     const form_span = document.querySelector('.form-span');
-    const checkbox = document.querySelector('.form-checkbox');
-    form_span.addEventListener('click', () => {
-        checkbox.checked = 'true';
-    });
+    const checkbox = document.querySelector('.form-checkbox'); debugger
+    if (checkbox.checked != true) {
+        form_span.addEventListener('click', () => {
+            checkbox.checked = 'true'; debugger
+        });
+    } else {
+        form_span.addEventListener('click', () => {
+            checkbox.checked = 'false'; debugger
+        });
+    }
+    
 });
